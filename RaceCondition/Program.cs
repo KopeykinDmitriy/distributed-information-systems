2ea163eda32bda912dada59180bec2ac8ef200e4
Dmitriy Kopeikin <d.kopeikin@bright-soft.ru>
2024-02-05 20:13:39 +0500

    RaceCondition finish

--- RaceCondition/Program.cs
+++ RaceCondition/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace RaceCondition;
-
-class Program
+﻿namespace RaceCondition
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+	class Program
+	{
+		private static int _counter = 0;
+		static void Main(string[] args)
+		{
+			var threadOne = new Thread(IncreaseCounter);
+			var threadTwo = new Thread(IncreaseCounter);
+			
+			threadOne.Start();
+			threadTwo.Start();
+			
+			Thread.Sleep(2000);
+			Console.WriteLine($"Ожидаемый результат counter = 20000\nФактический результат counter = {_counter}");
+		}
+
+		private static void IncreaseCounter()
+		{
+			for (var i = 0; i < 10000; i++)
+				_counter++;
+		}
+	}
 }
