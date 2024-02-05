b30f9bbdcb16826ce9c80eb661e309bc30c151b6
Dmitriy Kopeikin <d.kopeikin@bright-soft.ru>
2024-02-05 20:47:24 +0500

    DeadLock finish

---
+++ DeadLock/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace DeadLock
+{
+    class Program
+    {
+        private static object _lockOne = new();
+        private static object _lockTwo = new();
+        private static int _counterOne = 0;
+        private static int _counterTwo = 0;
+        
+        static void Main(string[] args)
+        {
+            var threadOne = new Thread(IncreaseCountersFirst);
+            var threadTwo = new Thread(IncreaseCountersSecond);
+            
+            threadOne.Start();
+            threadTwo.Start();
+
+            while (true)
+            {
+                Console.WriteLine($"{_counterOne}    {_counterTwo}");
+                Console.WriteLine($"{threadOne.ThreadState}");
+            }
+        }
+
+        private static void IncreaseCountersFirst()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                lock (_lockOne)
+                {
+                    _counterOne++;
+                    lock (_lockTwo)
+                    {
+                        _counterTwo++;
+                    }
+                }
+            }
+        }
+
+        private static void IncreaseCountersSecond()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                lock (_lockTwo)
+                {
+                    _counterTwo++;
+                    lock (_lockOne)
+                    {
+                        _counterOne++;
+                    }
+                }
+            }
+        }
+    }
+}
